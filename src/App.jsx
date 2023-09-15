fc7c3485c3013a44ed480a3d436b6335bf8fe9e0
PabloZottola <pablozottola66@gmail.com>
2023-09-15 14:26:31 -0300

    formulario creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import "./style/App.css";
 
 function App() {
-  return <></>;
+  return <>
+  <main>
+    <section className="register">
+    <img src="" alt="" />
+      <form>
+        <input type="text" />
+        <label htmlFor="">E-Mail</label>
+        <div>
+          <div>
+            <input type="text" />
+            <label htmlFor="">Nombre</label>
+            <input type="text" />
+            <label htmlFor="">Telefono</label>
+            <input type="text" />
+            <label htmlFor="">Confirmar contraseña</label>  
+          </div>
+          <div>
+            <input type="text" />
+            <label htmlFor="">Apellido</label>
+            <input type="text" />
+            <label htmlFor="">Fecha de nacimiento</label>
+            <input type="text" />
+            <label htmlFor="">Contraseña</label>
+          </div>
+        </div>
+      </form>
+    </section>
+  </main>
+  </>;
 }
 
 export default App;
