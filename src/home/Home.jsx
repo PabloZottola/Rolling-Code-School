2797ae6b86a7d2f39530f20b772659db9569aab7
Pablo Zottola <pablozottola66@gmail.com>
2023-09-18 21:07:58 -0300

    componente y carpeta para el home, creados

---
+++ src/home/Home.jsx
@@ -0,0 +1,11 @@
+function Index() {
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default Index;
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../auth/pages/Register";
+import Index from "../home/Home";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/index" element={<Index />} />
       </Routes>
     </BrowserRouter>
   );
