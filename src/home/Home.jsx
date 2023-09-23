1f50d26101bab9514ca49447e13c4eab7caa80c5
Pablo Zottola <pablozottola66@gmail.com>
2023-09-22 23:24:31 -0300

    home conectado con el login y funcionando

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,24 @@
-import Footer from "../components/Footer";
-import NavBar from "../components/NavBar";
+import { useState } from "react";
 import escuela1 from "../img/escuela1.jpg";
 import escuela2 from "../img/escuela2.jpg";
 import escuela3 from "../img/escuela3.jpg";
 import escuela4 from "../img/escuela4.jpg";
+import Logo from "../img/Logo.png";
+import login from "../img/usuario.png";
 import "../style/home.css";
+import "../style/navbar.css";
+import "../style/footer.css";
+import Login from "../auth/pages/Login";
 
 function Index() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleClick = () => {
+    console.log(isModalOpen);
+    if (isModalOpen) {
+      return setModalOpen(false);
+    } else setModalOpen(true);
+  };
   return (
     <>
       <div className="layout">
@@ -17,8 +29,31 @@ function Index() {
           <img src={escuela4} alt="" />
         </div>
       </div>
-      <NavBar />
-      <Footer />
+      {isModalOpen ? <Login></Login> : ""}
+      <header>
+        <div className="left">
+          <a href="">
+            <img src={Logo} alt="Logo de la escuela sin fondo" />
+          </a>
+        </div>
+        <ul>
+          <li>Nosotros</li>
+          <li>Contacto</li>
+          <li>Tienda</li>
+        </ul>
+        <div className="right">
+          <button onClick={handleClick}>
+            <img src={login} alt="Boton ingresar a su cuenta de usuario" />
+          </button>
+        </div>
+      </header>
+      <footer>
+        <ul>
+          <li>Rolling © 2023</li>
+          <li>Política de Privacidad</li>
+          <li>Contactanos</li>
+        </ul>
+      </footer>
     </>
   );
 }
