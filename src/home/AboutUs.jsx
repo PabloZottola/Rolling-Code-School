73c8e80c22315e9815ab370ed9a4f9c603e7e987
Julio jose Middagh <juliomiddagh@gmail.com>
2023-09-28 20:52:43 -0300

    responsive de h1, p y boton

--- src/home/AboutUs.jsx
+++ src/home/AboutUs.jsx
@@ -1,38 +1,36 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Button, Stack,  } from 'react-bootstrap';
 import "../style/aboutUs.css";
-import escuela4 from "../img/escuela4.jpg";
 
 function AboutUs() {
     return (
-        <body>
-            <div className="layout">
-                <NavBar />
-                <Container className="text-center">
-                    <Row>
-                        <Col className="position-relative d-flex flex-column align-items-center">
-                            <div className="overlay">
-                                <h1 className="custom-h1">Acerca de Nosotros</h1>
-                                <p className="text-justify custom-p">Descubre un mundo de posibilidades en Rolling School
-                                    En el corazón de Tucumán, emerge una escuela única que está transformando el futuro de la
-                                    educación. Bienvenidos a "Rolling School,"
-                                    donde los sueños se hacen realidad y las fronteras de la imaginación se desdibujan.
-                                    En Rolling School, no somos una escuela común. Somos un faro de innovación, una puerta 
-                                    de entrada a un mundo de tecnología y habilidades blandas.
-                                    Nuestro compromiso es guiar a nuestros estudiantes en un viaje de descubrimiento,
-                                    donde aprenden a dominar las herramientas del mañana y a cultivar las habilidades 
-                                    humanas que los destacarán en cualquier campo.</p>
-                                <Button variant="warning">Contactanos</Button>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Footer />
-                </Container>
-            </div>
-        </body>
+        <>
+        <div className="layout">
+            <Stack className="layout text-center align-items-center" gap={3}>
+                <div><h1 className="custom-h1 pt-15">¿Quiénes somos?</h1></div>
+                <div><p className="custom-p">Descubre un mundo de posibilidades en Rolling School
+                    En el corazón de Tucumán, emerge una escuela única que está transformando el futuro de la
+                    educación. Bienvenidos a "Rolling School,"
+                    donde los sueños se hacen realidad y las fronteras de la imaginación se desdibujan.
+                    En Rolling School, no somos una escuela común. Somos un faro de innovación, una puerta
+                    de entrada a un mundo de tecnología y habilidades blandas.
+                    Nuestro compromiso es guiar a nuestros estudiantes en un viaje de descubrimiento,
+                    donde aprenden a dominar las herramientas del mañana y a cultivar las habilidades
+                    humanas que los destacarán en cualquier campo.
+                </p></div>
+                <div><Button variant="warning">Contactanos</Button>
+                </div>
+            </Stack>
+        </div>
+        <Footer />
+        <NavBar />
+
+        </>
     );
 }
 
 export default AboutUs;
+
+
