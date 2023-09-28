2a99a629664e68b203a66cd1c888262aa6e70f15
Julio jose Middagh <juliomiddagh@gmail.com>
2023-09-28 10:50:15 -0300

    estructura basica AboutUs

---
+++ src/home/AboutUs.jsx
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+import AppContext from "../AppContext";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import Login from "../auth/pages/Login";
