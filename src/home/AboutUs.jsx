86c4a46e5ba9cde6efee815be570245945186c07
Julio jose Middagh <juliomiddagh@gmail.com>
2023-09-28 12:43:07 -0300

    Más intento de alinear sin exito al 100 los elementos

--- src/home/AboutUs.jsx
+++ src/home/AboutUs.jsx
@@ -12,13 +12,19 @@ function AboutUs() {
                 <NavBar />
                 <Container className="text-center">
                     <Row>
-                        <Col className="position-relative">
+                        <Col className="position-relative d-flex flex-column align-items-center">
                             <div className="overlay">
-                                <h1>Acerca de Nosotros</h1>
+                                <h1 className="custom-h1">Acerca de Nosotros</h1>
                                 <p className="text-justify custom-p">Descubre un mundo de posibilidades en Rolling School
-                                    En el corazón de Tucumán, emerge una escuela única que está transformando el futuro de la educación. Bienvenidos a "Rolling School," donde los sueños se hacen realidad y las fronteras de la imaginación se desdibujan.
-                                    En Rolling School, no somos una escuela común. Somos un faro de innovación, una puerta de entrada a un mundo de tecnología y habilidades blandas. Nuestro compromiso es guiar a nuestros estudiantes en un viaje de descubrimiento, donde aprenden a dominar las herramientas del mañana y a cultivar las habilidades humanas que los destacarán en cualquier campo.</p>
-                                <Button variant="success">Contactanos</Button>
+                                    En el corazón de Tucumán, emerge una escuela única que está transformando el futuro de la
+                                    educación. Bienvenidos a "Rolling School,"
+                                    donde los sueños se hacen realidad y las fronteras de la imaginación se desdibujan.
+                                    En Rolling School, no somos una escuela común. Somos un faro de innovación, una puerta 
+                                    de entrada a un mundo de tecnología y habilidades blandas.
+                                    Nuestro compromiso es guiar a nuestros estudiantes en un viaje de descubrimiento,
+                                    donde aprenden a dominar las herramientas del mañana y a cultivar las habilidades 
+                                    humanas que los destacarán en cualquier campo.</p>
+                                <Button variant="warning">Contactanos</Button>
                             </div>
                         </Col>
                     </Row>
@@ -26,7 +32,6 @@ function AboutUs() {
                 </Container>
             </div>
         </body>
-
     );
 }
 
